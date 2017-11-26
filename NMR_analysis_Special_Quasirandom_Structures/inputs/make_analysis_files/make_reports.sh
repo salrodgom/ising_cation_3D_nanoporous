0026fedd2be7e090a486ecadcd930f64d7ca6e24
Salvador Rodríguez-Gómez <salrodgom@upo.es>
2017-11-26 22:19:04 +0100

    modified:   make_reports.sh

--- NMR_analysis_Special_Quasirandom_Structures/inputs/make_analysis_files/make_reports.sh
+++ NMR_analysis_Special_Quasirandom_Structures/inputs/make_analysis_files/make_reports.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cc zeol_SiGe_distribution_environment_NMR_05.c -o zeol_SiGe_distribution_environment_NMR_05 -lm
-for nGe in $(seq 1 59) ; do
+for nGe in $(seq 1 1) ; do
  # Analysis:
  InputFile=configuration.${nGe}.txt
+ sed -i "s/\.cif//g" $InputFile
  ReportFile=report_${nGe}.txt
  nlines=$(wc -l ${InputFile} | awk '{print $1}')
  echo "${InputFile} ${nlines} ${nGe}
